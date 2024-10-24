95dae5d1fad25f16f4486715454c45e3f3bd1b8a
Sandi Junaedi <junaedisandi784@gmail.com>
2024-10-24 22:29:14 +0700

    kamis 24 Oktober 2024 Belajar sesi I, mengubah file input otput c++ menjadi sebuah program formulir sederhana.

--- input.cpp
+++ input.cpp
@@ -1,3 +1,8 @@
+//Berikut ini adalah contoh program sederhana yang memanfaatkan 
+//cout dan cin sebagai input dan output.
+
+
+
 #include <iostream>
 
 using namespace::std;
@@ -5,19 +10,37 @@ using namespace::std;
 int main() {
 
 	char nama[51] = {};
-	char hoby[51]={};
+	char jenisK [11] = {};
+	char tempatLahir [41] = {};
+	char tanggalLahir [11] = {};
+	int beratBadan ;
+	int tinggiBadan ;
+	
 
-	cout << "============= Contoh program input ============="<<endl;
-	cout << "masukan nama anda : ";
+	cout << "============= Contoh program input formulir sederhana ============="<<endl;
+	cout << "Nama : ";
 	cin.getline(nama, 51) ;
-	cout << "apa hobby mu : ";
-	cin. getline(hoby, 51) ;
+	cout << "Jenis kelamin : ";
+	cin. getline( jenisK, 51) ;
+	cout << "Tempat lahir : ";
+        cin.getline( tempatLahir, 41) ;
+        cout << "Tanggal lahir : ";
+        cin. getline( tanggalLahir, 11) ;
+	cout << "Berat badan : ";
+        cin >> beratBadan ;
+        cout << "Tinggi badan : ";
+        cin >> tinggiBadan ;
 
 
-	cout << endl <<"==========================";    cout << endl << "Hai " << nama <<" Selamat datang di Cpp! "; 
-	cout << endl << "kamu memiliki hobi " << hoby << " ? Wow 🙊 kamu bisa membuat program berdasarkan hobby mu di cpp! " <<endl;
-
-
+	cout << endl <<"================================================"<<endl;
+    	cout << "Terimakasih telah mengisi formulir ini."<<endl<<"Berikut adalah data yang kamu masukan. silakan cek kembali! " <<endl;
+       
+	cout << "Nama : " << nama <<endl;
+	cout << "Jenis kelamin : " << jenisK <<endl;
+	cout << "Tempat lahir : " << tempatLahir <<endl;
+	cout << "Tanggal lahir : " << tanggalLahir <<endl;
+        cout << "Berat badan : " << beratBadan<<" kg" <<endl;
+        cout << "Tinggi badan : " << tinggiBadan<<" cm" <<endl;
 
 
 	return 0;
